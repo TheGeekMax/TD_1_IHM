1278ef1e27d1aa8a2ebcf16983b74c579b40382f
TheGeekMax <maximesanciaume@gmail.com>
2025-01-13 08:55:32 +0100

    working active Bird and BirdController

--- src/main/java/ensisa/birds/MainController.java
+++ src/main/java/ensisa/birds/MainController.java
@@ -1,4 +1,31 @@
 package ensisa.birds;
 
+import ensisa.birds.model.Bird;
+import ensisa.birds.model.BirdRepository;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class MainController {
+    // data
+    public Bird currentBird;
+    private BirdRepository repository;
+
+
+    // components
+    @FXML
+    private Label commonNameLabel;
+    @FXML
+    private Label latinNameLabel;
+
+    public MainController() {
+        repository = new BirdRepository();
+        repository.load();
+
+        currentBird = repository.birds.get(0);
+    }
+
+    public void initialize() {
+        commonNameLabel.textProperty().bind(currentBird.commonNameProperty());
+        latinNameLabel.textProperty().bind(currentBird.latinNameProperty());
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,11 @@
 module ensisa.birds {
-    requires javafx.controls;
+    requires transitive javafx.controls;
     requires javafx.fxml;
 
+    requires com.fasterxml.jackson.databind;
+    requires jdk.compiler;
 
-    opens ensisa.birds to javafx.fxml;
+    opens ensisa.birds to javafx.fxml, com.fasterxml.jackson.databind;
     exports ensisa.birds;
+    exports ensisa.birds.model;
 }
