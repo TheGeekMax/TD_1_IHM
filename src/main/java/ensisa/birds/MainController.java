7a3a0604a9999f7ca7195216ba1b4bbd89592295
TheGeekMax <maximesanciaume@gmail.com>
2025-01-13 10:20:08 +0100

    added working left menu

--- src/main/java/ensisa/birds/MainController.java
+++ src/main/java/ensisa/birds/MainController.java
@@ -2,12 +2,17 @@ package ensisa.birds;
 
 import ensisa.birds.model.Bird;
 import ensisa.birds.model.BirdRepository;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
 
 public class MainController {
     // data
-    public Bird currentBird;
+    private final ObjectProperty<Bird> currentBird;
     private BirdRepository repository;
 
 
@@ -16,16 +21,60 @@ public class MainController {
     private Label commonNameLabel;
     @FXML
     private Label latinNameLabel;
+    @FXML
+    private Label familyLabel;
+    @FXML
+    private Label genusLabel;
+    @FXML
+    private Label specieLabel;
+    @FXML
+    private Label descriptionLabel;
+    @FXML
+    private ImageView birdImageView;
+    @FXML
+    private ListView<Bird> birdListView;
+    @FXML
+    private VBox birdView;
+
 
     public MainController() {
         repository = new BirdRepository();
         repository.load();
 
-        currentBird = repository.birds.get(0);
+        currentBird = new SimpleObjectProperty<>(repository.birds.get(0));
+    }
+
+    public void bind(Bird bird) {
+        commonNameLabel.textProperty().bind(bird.commonNameProperty());
+        latinNameLabel.textProperty().bind(bird.latinNameProperty());
+        familyLabel.textProperty().bind(bird.familyProperty());
+        genusLabel.textProperty().bind(bird.genusProperty());
+        specieLabel.textProperty().bind(bird.specieProperty());
+        descriptionLabel.textProperty().bind(bird.descriptionProperty());
+        birdImageView.imageProperty().bind(bird.imageProperty());
     }
 
     public void initialize() {
-        commonNameLabel.textProperty().bind(currentBird.commonNameProperty());
-        latinNameLabel.textProperty().bind(currentBird.latinNameProperty());
+        birdListView.setCellFactory(new BirdCellFactory());
+        birdListView.setItems(repository.birds);
+        currentBirdProperty().bind(birdListView.getSelectionModel().selectedItemProperty());
+        currentBirdProperty().addListener((o, oldValue, newValue) -> {
+            if (oldValue != null) {
+                bind(newValue);
+            }
+        });
+        birdView.visibleProperty().bind(currentBirdProperty().isNotNull());
+    }
+
+    private Bird getCurrentBird() {
+        return currentBird.get();
+    }
+
+    public void setCurrentBird(Bird currentBird) {
+        this.currentBird.set(currentBird);
+    }
+
+    public ObjectProperty<Bird> currentBirdProperty() {
+        return currentBird;
     }
 }
--- src/main/java/ensisa/birds/model/Bird.java
+++ src/main/java/ensisa/birds/model/Bird.java
@@ -1,7 +1,11 @@
 package ensisa.birds.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import javafx.beans.property.Property;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.scene.image.Image;
 
 public class Bird {
     private StringProperty family = new SimpleStringProperty(this, "family", "");
@@ -12,89 +16,110 @@ public class Bird {
     private StringProperty description = new SimpleStringProperty(this, "description", "");
     private StringProperty imagePath = new SimpleStringProperty(this, "imagePath", "");
 
-    public String getFamily() {
-        return family.get();
+    @JsonIgnore
+    private Property<Image> image = new SimpleObjectProperty<>(this, "image");
+
+    public Bird() {
+        imagePath.addListener((v, oldValue, newValue) -> {
+            var url = getClass().getResource("/ensisa/birds/assets/images/" + getImagePath()).toExternalForm();
+            setImage(new Image(url));
+        });
     }
 
-    public StringProperty familyProperty() {
-        return family;
+    public Image getImage() {
+        return image.getValue();
+    }
+
+    public void setImage(Image image) {
+        this.image.setValue(image);
+    }
+
+    public Property<Image> imageProperty() {
+        return image;
+    }
+
+    public String getFamily() {
+        return family.get();
     }
 
     public void setFamily(String family) {
         this.family.set(family);
     }
 
+    public StringProperty familyProperty() {
+        return family;
+    }
 
     public String getGenus() {
         return genus.get();
-    }
-
-    public StringProperty genusProperty() {
-        return genus;
     }
 
     public void setGenus(String genus) {
         this.genus.set(genus);
     }
 
-    public String getSpecie() {
-        return specie.get();
+    public StringProperty genusProperty() {
+        return genus;
     }
 
-    public StringProperty specieProperty() {
-        return specie;
+    public String getSpecie() {
+        return specie.get();
     }
 
     public void setSpecie(String specie) {
         this.specie.set(specie);
     }
 
-    public String getCommonName() {
-        return commonName.get();
+    public StringProperty specieProperty() {
+        return specie;
     }
 
-    public StringProperty commonNameProperty() {
-        return commonName;
+    public String getCommonName() {
+        return commonName.get();
     }
 
     public void setCommonName(String commonName) {
         this.commonName.set(commonName);
     }
 
-    public String getLatinName() {
-        return latinName.get();
+    public StringProperty commonNameProperty() {
+        return commonName;
     }
 
-    public StringProperty latinNameProperty() {
-        return latinName;
+    public String getLatinName() {
+        return latinName.get();
     }
 
     public void setLatinName(String latinName) {
         this.latinName.set(latinName);
     }
 
-    public String getDescription() {
-        return description.get();
+    public StringProperty latinNameProperty() {
+        return latinName;
     }
 
-    public StringProperty descriptionProperty() {
-        return description;
+    public String getDescription() {
+        return description.get();
     }
 
     public void setDescription(String description) {
         this.description.set(description);
     }
 
+    public StringProperty descriptionProperty() {
+        return description;
+    }
+
     public String getImagePath() {
         return imagePath.get();
     }
 
-    public StringProperty imagePathProperty() {
-        return imagePath;
-    }
-
     public void setImagePath(String imagePath) {
         this.imagePath.set(imagePath);
+    }
+
+    public StringProperty imagePathProperty() {
+        return imagePath;
     }
 
 
